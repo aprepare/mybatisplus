4dffee899398345867a3bf0fb84e7f185a0e3c9d
prepare <57052177+zcdsg125@users.noreply.github.com>
2023-06-26 15:34:22 +0800

    测试

--- mybatisplus_04_generator/src/main/java/com/aaa/controller/UserController.java
+++ mybatisplus_04_generator/src/main/java/com/aaa/controller/UserController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    public static void main(String[] args) {
+        System.out.println("测试用的");
+    }
 }
 
